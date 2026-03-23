7e3d06294927e01d8ef1278439954b98519323ac
Abantika puty <pooja.puty@gmail.com>
2026-03-24 03:13:31 +0530

    change

--- cypress.config.js
+++ cypress.config.js
@@ -1,25 +1,26 @@
-const { defineConfig } = require("Cypress");
+const { defineConfig } = require("cypress");  // ✅ FIXED
 
 module.exports = defineConfig({
   video: true,
   screenshotOnRunFailure: true,
-  html: true,
+
   reporter: 'cypress-mochawesome-reporter',
 
   reporterOptions: {
-  reportDir: 'reports',
-  overwrite: false,
-  html: true,
-  json: true
-},
+    reportDir: 'reports',
+    overwrite: false,
+    html: true,
+    json: true
+  },
 
   e2e: {
     baseUrl: 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login',
+
     specPattern: "cypress/e2e/**/*.js",
+
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
-      return require('./cypress/plugins/index.js')(on, config)
-      
+      return require('./cypress/plugins/index.js')(on, config);
     },
   },
 
